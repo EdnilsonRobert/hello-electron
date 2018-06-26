594cd65183d28634c918cad1331f1a54c12f76e5
Ednilson Robert <ednilson.robert@rockt.com.br>
2018-06-26 20:06:50 -0300

    Commitando meu ovo.

---
+++ main.js
@@ -0,0 +1,30 @@
+const {app, BrowserWindow} = require('electron');
+
+let win
+
+function createWindow() {
+    win = new BrowserWindow({
+        width: 800,
+        height: 600,
+        // frame: false
+    });
+    win.loadFile('./egg/my-egg.html');
+    // win.webContents.openDevTools()
+    win.on('closed', function() {
+        win = null;
+    });
+}
+
+app.on('ready', createWindow);
+
+app.on('window-all-closed', function() {
+    if(process.platform !== 'darwin') {
+        app.quit();
+    }
+});
+
+app.on('activate', function() {
+    if(win === null) {
+        createWindow();
+    }
+});
